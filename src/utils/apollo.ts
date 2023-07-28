48328f8d078139d27239baa47105fc253532c67d
ahmed <ahmed.j.lazher@gmail.com>
2023-07-28 15:10:38 +0100

    gql no cache

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -3,12 +3,23 @@ import {
   InMemoryCache,
   ApolloLink,
   HttpLink,
+  DefaultOptions,
 } from "@apollo/client";
 
 const TOKEN = process.env.CONTENTFUL_TOKEN;
 const SPACE = process.env.CONTENTFUL_SPACE;
 const URI = `https://graphql.contentful.com/content/v1/spaces/${SPACE}`;
-
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  };
+  
 const http = new HttpLink({
   uri: URI,
   headers: {
@@ -24,6 +35,7 @@ const cache = new InMemoryCache({ resultCaching: false });
 const apolloClient = new ApolloClient({
   link,
   cache,
+  defaultOptions
 });
 
 export default apolloClient;
